9a1e87d96ce5f3b7588c834cff835e77b7aa1ed5
Spartak Katvickii <bonus2k@gmail.com>
2020-12-20 20:12:56 +0400

    lesson 6

--- src/main/java/ru/snkatvit/springcourse/MusicPlayer.java
+++ src/main/java/ru/snkatvit/springcourse/MusicPlayer.java
@@ -1,13 +1,41 @@
 package ru.snkatvit.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
+    private String name;
+    private int volume;
 
     public MusicPlayer(Music music) {
-        this.music = music;
+         this.musicList.add(music);
+    }
+
+    public MusicPlayer() {
     }
 
     public void playMusic(){
-        System.out.println("Playing: "+music.getSong());
+        musicList.forEach(o-> System.out.println("Playing: " + o.getSong()));
+    }
+
+    public void setMusic(List<Music> musicList) {
+        this.musicList=musicList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 }
